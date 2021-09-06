b81b3ce8a701ccaa47dd58b3ab893ad7ce3a34df
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2021-09-06 20:15:38 +0100

    Add IUPred Cut script

---
+++ scripts/iupredCut.py
@@ -0,0 +1,31 @@
+import sys
+import os.path as path
+
+max_length = int(sys.argv[2]) #Must be even
+name = sys.argv[1][:-len(".fasta.iupred")]
+path_head, path_tail = path.split(name)
+file = open(sys.argv[1])
+lines = file.read().splitlines()
+file.close()
+residues = []
+scores = []
+for i in range(len(lines)):
+	data = lines[i].split("\t")
+	residues.append(data[1])
+	scores.append(float(data[2]))
+
+offset = 0
+while len(residues) > max_length:
+	max_score = max(scores[int(max_length/2):max_length])
+	index = scores[int(max_length/2):max_length].index(max_score) + int(max_length/2)
+	file = open(f"{name}_{offset+1}-{index}.fasta", "w")
+	file.write(f">{path_tail}_{offset+1}-{index}\n")
+	file.write(f"{''.join(residues[:index])}\n")
+	file.close()
+	offset = offset + index
+	residues = residues[index:]
+	scores = scores[index:]
+file = open(f"{name}_{offset+1}-{offset+len(residues)}.fasta", "w")
+file.write(f">{path_tail}_{offset+1}-{offset+len(residues)}\n")
+file.write(f"{''.join(residues)}\n")
+file.close()
