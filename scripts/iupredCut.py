a55b28e565ccfc0aa29ff25fd58bce6c89d351e0
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2021-09-12 00:13:19 +0100

    Add patch test/addition

--- scripts/iupredCut.py
+++ scripts/iupredCut.py
@@ -1,7 +1,9 @@
 import sys
 import os.path as path
 
-max_length = int(sys.argv[2]) #Must be even
+max_length = int(sys.argv[2]) # Max length of sequence, must be even
+patch_size = 30 # Region around cuts in which to check score
+patch_thr = 0.5 # Score threshold for making patch
 name = sys.argv[1][:-len(".fasta.iupred")]
 path_head, path_tail = path.split(name)
 file = open(sys.argv[1])
@@ -13,6 +15,7 @@ for i in range(len(lines)):
 	data = lines[i].split("\t")
 	residues.append(data[1])
 	scores.append(float(data[2]))
+residues_raw = residues
 
 offset = 0
 # Allow 10% over-length on final section
@@ -23,6 +26,16 @@ while len(residues) > max_length * 1.1:
 	file.write(f">{path_tail}_{offset+1}-{offset+index}\n")
 	file.write(f"{''.join(residues[:index])}\n")
 	file.close()
+	# Check cut region, and write spanning 'patch' if iupred score is too low
+	min_score_patch = min(scores[max(index - 30, 0):min(index, len(residues))])
+	if min_score_patch < patch_thr:
+		patch_start = max(index - max_length / 4, 0)
+		patch_end = in(index + max_length / 4, len(residues))
+		file = open(f"{name}_{patch_start+1}-{patch_end}.fasta", "w")
+		file.write(f">{path_tail}_{patch_start+1}-{patch_end}\n")
+		file.write(f"{''.join(residues_raw[patch_start:patch_end])}\n")
+		file.close()
+	# Update offsets for next iteration
 	offset = offset + index
 	residues = residues[index:]
 	scores = scores[index:]
