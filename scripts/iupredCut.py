ce329006f165854ca59bebc33196b7e31e8ab66e
Richard Wheeler <2762690+zephyris@users.noreply.github.com>
2021-09-12 18:11:38 +0100

    Specify patch size independent of max length

--- scripts/iupredCut.py
+++ scripts/iupredCut.py
@@ -4,6 +4,7 @@ import os.path as path
 max_length = int(sys.argv[2]) # Max length of sequence, must be even
 patch_size = 30 # Region around cuts in which to check score
 patch_thr = 0.25 # Score threshold for making patch
+patch_length = 200 # Patch overlap (half full patch length)
 name = sys.argv[1][:-len(".fasta.iupred")]
 path_head, path_tail = path.split(name)
 file = open(sys.argv[1])
@@ -18,8 +19,7 @@ for i in range(len(lines)):
 residues_raw = residues
 
 offset = 0
-# Allow 10% over-length on final section
-while len(residues) > max_length * 1.1:
+while len(residues) > max_length:
 	max_score = max(scores[int(max_length/2):max_length])
 	index = scores[int(max_length/2):max_length].index(max_score) + int(max_length/2)
 	file = open(f"{name}_{offset+1}-{offset+index}.fasta", "w")
@@ -29,8 +29,8 @@ while len(residues) > max_length * 1.1:
 	# Check cut region, and write spanning 'patch' if iupred score is too low
 	min_score_patch = min(scores[max(index - 30, 0):min(index, len(residues))])
 	if min_score_patch < patch_thr:
-		patch_start = int(max(index - max_length / 4, 0))
-		patch_end = int(min(index + max_length / 4, len(residues)))
+		patch_start = int(max(index - patch_length, 0))
+		patch_end = int(min(index + patch_length, len(residues)))
 		file = open(f"{name}_{offset + patch_start+1}-{offset + patch_end}.fasta", "w")
 		file.write(f">{path_tail}_{offset + patch_start+1}-{offset + patch_end}\n")
 		file.write(f"{''.join(residues_raw[offset + patch_start:offset + patch_end])}\n")
